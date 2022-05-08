d01aafd05926af7ac9f17bc45333e4db6a747542
Chenyang Zhu <zhu.klaus@gmail.com>
2022-05-08 08:35:26 -0400

    add covariate codes

--- src/codes/matrix_completion.R
+++ src/codes/matrix_completion.R
@@ -2,7 +2,7 @@
 library(MCPanel)
 
 M <- read.csv("./src/data/synthetic_matrix.csv")
-X <- as.matrix(M[, 4:8])
+X <- as.matrix(M[, 4:7])
 treated_index <- !is.na(M$funding_round_num)
 
 mask <- X * 0
@@ -11,7 +11,7 @@ mask <- X * 0
 for (i in 1:nrow(X)){
   if (!is.na(M$funding_round_num[i])){
     idx = M$funding_round_num[i]
-    for (j in (idx-1):5){
+    for (j in (idx-1):4){
       if (j != 0){ # b/c we deliberately delete rows
         mask[i, j] = 1
       }
@@ -19,19 +19,31 @@ for (i in 1:nrow(X)){
   }
 }
 
+mask
+
 estimated_obj <- mcnnm_cv(X, mask, to_estimate_u = 1, num_lam_L = 40)
 best_lam_L <- estimated_obj$best_lambda
 estimated_mat <- estimated_obj$L
 estimated_mat
-
-# The estimation
-avg_difference = rowMeans(X * mask - estimated_mat)
-hist(avg_difference[treated_index])
-
 write.csv(estimated_mat, "./src/data/Emat.csv")
 
 
+# The Placebo Test
+X <- as.matrix(M[, 4:7]) 
+X.untreated <- X[is.na(M$funding_round_num), ]
+X.untreated
 
-trEffect = read.csv("./src/data/TrEffect.csv")
-t.test(trEffect)
+synthetic_X <- X.untreated * 0
 
+for (i in 1:nrow(X.untreated)){
+# for (i in 1:10){
+  print(i)
+  mask <- X.untreated * 0
+  mask[i, ] = c(1,1,1,1)
+  estimated_obj <- mcnnm_cv(X.untreated, mask, to_estimate_u = 1, num_lam_L = 1)
+  estimated_mat <- estimated_obj$L
+  synthetic_X[i, ] <- estimated_mat[i, ]
+}
+
+sum(synthetic_X)
+synthetic_X
